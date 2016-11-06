56a399269e375d8d43e74bec299ed0d0fe174524
jngd <juangrdiaz@gmail.com>
2016-11-06 14:46:23 +0100

    T19E01 structure added

---
+++ T19E01/T19E01/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  T19E01
+//
+//  Created by jngd on 06/11/16.
+//  Copyright © 2016 jngd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+}
+
