7680b241121da8b60bfa468709752aa2a35577f8
jngd <juangrdiaz@gmail.com>
2016-11-06 23:14:11 +0100

    T19E02 added

--- T19E02/T19E02/ViewController.swift
+++ T19E02/T19E02/ViewController.swift
@@ -1,25 +1,110 @@
-//
-//  ViewController.swift
-//  T19E02
-//
-//  Created by jngd on 06/11/16.
-//  Copyright © 2016 jngd. All rights reserved.
-//
+/**
+* Copyright (c) 2016 Juan Garcia
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Movie {
+	let name: String
+	let director: String
+	let year: Int
+	let image: String
+}
 
+class ViewController: UIViewController {
+	
+	/***** Outlets *****/
+	@IBOutlet weak var table: UITableView!
+	
+	/***** Vars *****/
+	var movies: [Movie] = []
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		loadMovies()
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
+	
+	func loadMovies(){
+		movies = [Movie]()
+		
+		let db_path = NSBundle.mainBundle().pathForResource("peliculas", ofType:
+			"sqlite")
+		
+		var db = COpaquePointer()
+		let status = sqlite3_open(db_path!, &db)
+		
+		if status != SQLITE_OK {
+			print("Error at open DB")
+			return
+		}
+		
+		let query_stmt = "SELECT * FROM peliculas"
+		var statement = COpaquePointer()
+		
+		if(sqlite3_prepare_v2(db, query_stmt, -1, &statement, nil) !=
+			SQLITE_OK){
+			print("Error in prepare")
+			return
+		}
+		
+		while(sqlite3_step(statement) == SQLITE_ROW){
+			let nameMem = sqlite3_column_text(statement, 1)
+			let name = String.fromCString(UnsafePointer<CChar>(nameMem))
+			let directorMem = sqlite3_column_text(statement, 2)
+			let director =
+				String.fromCString(UnsafePointer<CChar>(directorMem))
+			let year = sqlite3_column_int(statement, 3)
+			let imagenMem = sqlite3_column_text(statement, 4)
+			let imagen =
+				String.fromCString(UnsafePointer<CChar>(imagenMem))
+			let movie = Movie(name: name!, director: director!,
+			                  year: Int(year), image: imagen!)
+			movies.append(movie)
+		}
+		
+		sqlite3_finalize(statement)
+		
+		print("\(movies.count)")
+		self.table.reloadData()
+	}
+}
 
-
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+		cell.textLabel!.text = movies[indexPath.item].name
+		return cell
+	}
+	
+	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return movies.count
+	}
 }
 
